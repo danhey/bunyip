f18e4c494052d9ea03a19d0710c9cbc863d4d2f1
Unknown <danielhey@outlook.com>
2019-12-05 15:58:29 -0500

    Comments

--- bunyip/bunyip.py
+++ bunyip/bunyip.py
@@ -2,6 +2,7 @@
 from __future__ import division, print_function
 
 from .knn import KNN
+from .geometry import solve_geometry, from_geometry
 
 import numpy as np
 import ellc
@@ -27,15 +28,14 @@ class Bunyip:
         flux_err : np.ndarray, optional
             Errors associated with flux, by default None
         """
+        # Assign initial values
         self.phase = phase
         self.flux = flux / np.median(flux)
         if flux_err is None:
             flux_err = np.zeros_like(flux)
         self.flux_err = flux_err
-
-        # Initialise network
-        self.model = self.initialize_network()
         self.period = period
+
         # Initialise fit parameters
         self.parameters = {
             'q':    1.,
@@ -56,11 +56,13 @@ class Bunyip:
             # 'ldc_2': 0.5,
         }
 
+        # Parameters specific to ellc
         self.ellc_parameters = {
             'shape_1': shape_1,
             'shape_2': shape_2
         }
         
+        # Some wrappers for the optimizers. don't @ me
         self.nll = lambda *args: -self.lnlike(*args)
         self.nll_wrapper = lambda *args: -self.lnlike_wrapper(*args)
 
@@ -93,8 +95,9 @@ class Bunyip:
             [description]
         """
         if period is None:
-            raise ValueError('I really should calculate the period')
-
+            raise ValueError('I really should calculate the period here.')
+        
+        # Fold and sort the light curve
         phase = (t0 % period) / period    
         fold_time = (((time - phase * period) / period) % 1)
         fold_time[fold_time > 0.5] -= 1
@@ -103,13 +106,13 @@ class Bunyip:
         if flux_err is None:
             flux_err = np.zeros_like(time)
 
+        # Bunyip needs args
         return Bunyip(fold_time[sorted_args], 
                         flux[sorted_args], 
                         flux_err[sorted_args],
                         period=period)
 
-    def lc_model(self, time=None):
-            
+    def lc_model(self):
         rsum = self.parameters['rsum']
         rratio = self.parameters['rratio']
         r1, r2 = rsum/(1.+rratio), rsum*rratio/(1.+rratio)
@@ -135,7 +138,6 @@ class Bunyip:
                     #  ldc_1=self.parameters['ldc_1'],
                     #  ldc_2=self.parameters['ldc_2']
                     )
-
         return lc + self.parameters['mean']
     
     def initialize_network(self, model_path=None):
@@ -153,12 +155,27 @@ class Bunyip:
         
         with CustomObjectScope({'GlorotUniform': glorot_uniform()}):
             model = load_model(model_path)
-        
         return model
         
     def predict(self, binned_flux, model=None, norm=True):
+        """Predict light curve parameters from the neural network
+        
+        Parameters
+        ----------
+        binned_flux : np.ndarray
+            Flux values
+        model : [type], optional
+            [description], by default None
+        norm : bool, optional
+            [description], by default True
+        
+        Returns
+        -------
+        [type]
+            [description]
+        """
         if model is None:
-            model = self.model
+            model = self.initialize_network()
         y_hat = model.predict(binned_flux[None,:])
             
         if norm:
@@ -184,7 +201,15 @@ class Bunyip:
         return y_hat
    
     def update_from_geometry(self, diagnose=False):
-        from .geometry import solve_geometry, from_geometry
+        """Update light curve parameters from geometric information contained
+        within the eclipse times and depths.
+        
+        Parameters
+        ----------
+        diagnose : bool, optional
+            Whether to show a diagnostic plot of estimate eclipse ingress and 
+            egress positions, by default False
+        """
         geometry_vals = solve_geometry(self.phase, self.flux, self.period, t0=self.parameters['t0'], diagnose=diagnose)
         dphi = np.abs(geometry_vals['t0_primary'] - geometry_vals['t0_secondary'])
         ecc, per0 = from_geometry(dphi)
@@ -195,13 +220,12 @@ class Bunyip:
         })
 
     def update_from_knn(self):
+        """Update light curve parameters from KNN model
+        """
         model = KNN()
         lc = lk.LightCurve(self.phase, self.flux)
         binned = lc.bin(bins=101).normalize()
-        # plt.plot(binned.flux)
         prediction = model.predict(binned.flux)[0]
-        # db_param_names = ['rsum', 'rratio', 'eccen', 'omega', 'incl', 'q', 'T1', 'tratio']
-        # rsum, rratio, ecc, per0, incl, q, t1, tratio = prediction
         q, incl, rsum, rratio, t1, tratio, ecc, per0 = prediction
         self.parameters.update({
             'q': q,
@@ -215,12 +239,23 @@ class Bunyip:
         # self.update_parameters(prediction)
 
     def update_from_network(self, **kwargs):
+        """Update light curve parameters from neural network model.
+        """
         lc = lk.LightCurve(self.phase, self.flux)
         binned = lc.bin(bins=100).normalize()
         prediction = self.predict(binned.flux, **kwargs)[0]
         self.update_parameters(prediction)
     
     def update_parameters(self, prediction):
+        """Update parameters from a prediction given by either the neural
+        network, or the KNN classifier
+        
+        Parameters
+        ----------
+        prediction : list
+            List of prediction values, should be in the form 
+            q, r1, r2, tratio, incl, ecc, per0
+        """
         old_params = self.parameters.copy()
         q, r1, r2, tratio, incl, ecc, per0 = prediction
         self.parameters.update({
@@ -239,6 +274,13 @@ class Bunyip:
             print('The network failed to find a solution, defaulting to original values. Or grid search here?')
 
     def lnprior(self):
+        """log prior of the ellc model
+        
+        Returns
+        -------
+        float
+            The prior probability
+        """
         rsum = self.parameters['rsum']
         rratio = self.parameters['rratio']
         r1, r2 = rsum/(1.+rratio), rsum*rratio/(1.+rratio)
@@ -246,7 +288,7 @@ class Bunyip:
         ecc = self.parameters['fc']**2 + self.parameters['fs']**2
         per0 = np.arctan2(self.parameters['fs'],self.parameters['fc'])
         if ((0 < self.parameters['q']) & (self.parameters['incl'] <= 90) & (0 < self.parameters['sbratio']) & 
-            ((-0.5) <= self.parameters['t0'] <= (0.5)) & (0 < r1 < 1) & (0 < r2 < 1) & (0 <= ecc < 1)):
+            ((-0.5) <= self.parameters['t0'] <= (0.5)) & (0 < r1 < 1) & (0 < r2 < 1) & (0 <= ecc < 1.)):
             return 0.
         else:
             return -np.inf
@@ -308,6 +350,23 @@ class Bunyip:
         return soln
     
     def differential_evolution(self, bounds):
+        """Global optimization with differential evolution. Costly, but effective!
+        
+        Parameters
+        ----------
+        bounds : list
+            List of bounds for each parameters (i.e. [(0,1), (2,5)])
+        
+        Returns
+        -------
+        dict
+            Results of the optimization
+        
+        Raises
+        ------
+        ValueError
+            As many bounds as parameters must be passed.
+        """
         vars = list(self.parameters.keys())
 
         if not len(vars) == len(bounds):
@@ -319,7 +378,19 @@ class Bunyip:
             self.parameters.update({key:val})
         return soln
         
-    def plot_model(self, ax=None):
+    def plot_model(self, ax=None, **kwargs):
+        """Plot the current model in the parameters dict
+        
+        Parameters
+        ----------
+        ax : matplotlib axis, optional
+            axis object on which to plot, by default None
+        
+        Returns
+        -------
+        matplotlib axis
+            axis
+        """
         model_flux = self.lc_model()
         
         if ax is None:
@@ -332,6 +403,22 @@ class Bunyip:
         return ax
     
     def run_emcee(self, burnin=1000, draws=2000, nwalkers=32, **kwargs):
+        """Run emcee initialised around the current parameters
+        
+        Parameters
+        ----------
+        burnin : int, optional
+            Number of burn-in values, by default 1000
+        draws : int, optional
+            Number of draws, by default 2000
+        nwalkers : int, optional
+            Number of walkers in the chain, by default 32
+        
+        Returns
+        -------
+        [type]
+            [description]
+        """
         import emcee
         vars = list(self.parameters.keys())
         init = list(self.parameters.values())
@@ -348,6 +435,7 @@ class Bunyip:
         return samples
 
     def corner_plot(self, trace, **kwargs):
+        
         import corner
         ax = corner.corner(trace, labels=list(self.parameters.keys()),
                     show_titles=True, **kwargs);
@@ -364,6 +452,16 @@ class Bunyip:
             model_flux = self.lc_model()
             plt.plot(self.phase, model_flux, **kwargs)
         return ax
+
+    def get_model_from_trace(self, trace, n=100):
+        vars = list(self.parameters.keys())
+        samples = trace[np.random.choice(len(trace), size=n)]
+        lcs = []
+        for i in samples:
+            [self.parameters.update({key:val}) for key, val in zip(vars, i)]
+            model_flux = self.lc_model()
+            lcs.append(model_flux)
+        return lcs
 
     def to_phoebe(self):
         pass
